81b2ae963384b33bc4f3b835526faf530d490ddf
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2020-03-22 15:36:09 +0800

    add basic

---
+++ 001-node-vars.ts
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+console.log(Date);
+console.log(Math);
+console.log(setTimeout);
+console.log(setInterval);
+
+// not exists in node env
+// console.log(requestAnimationFrame);
+console.log(setImmediate);
+
+// current script file and script's dir
+console.log(__filename);
+console.log(__dirname);
+
+console.log(process);
+
+console.log(process.cpuUsage());
+console.log(process.memoryUsage());
+console.log(process.hrtime());
+console.log(process.kill);
+console.log(process.env);
+console.log(process.env.PATH);
+console.log(process.argv);
