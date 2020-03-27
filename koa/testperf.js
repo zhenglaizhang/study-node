af950618b20675830a7b66a68aa334f1539433a7
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2020-03-27 22:39:29 +0800

    perf tuning phase 3

--- koa/testperf.js
+++ koa/testperf.js
@@ -4,13 +4,15 @@ const fs = require("fs");
 const path = require("path");
 const app = new koa();
 
-const body = fs.readFileSync(
-  path.join(__dirname, "static/home/index.htm"),
-  "utf-8"
-);
+// ab -c100 -t15 "http://localhost:3000/"
+
+// by default return buf instead of utf-8
+const body = fs.readFileSync(path.join(__dirname, "static/home/index.htm"));
 
 app.use(
   mount("/", async ctx => {
+    ctx.status = 200;
+    ctx.stype = "html";
     ctx.body = body;
   })
 );
