a73766ba721662afce2380bb70f2af4f8396810f
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2020-03-27 22:33:19 +0800

    perf tuning phase 2

--- koa/testperf.js
+++ koa/testperf.js
@@ -4,12 +4,14 @@ const fs = require("fs");
 const path = require("path");
 const app = new koa();
 
+const body = fs.readFileSync(
+  path.join(__dirname, "static/home/index.htm"),
+  "utf-8"
+);
+
 app.use(
   mount("/", async ctx => {
-    ctx.body = fs.readFileSync(
-      path.join(__dirname, "static/home/index.htm"),
-      "utf-8"
-    );
+    ctx.body = body;
   })
 );
 
