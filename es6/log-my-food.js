eb5f3778c2141b24ab6241d128f272b928467e18
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2020-03-30 22:38:57 +0800

    customize iterable

--- es6/log-my-food.js
+++ es6/log-my-food.js
@@ -6,19 +6,56 @@ const axios = require("axios");
 
 const readline = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  prompt: "enter command > "
 });
 
-readline.question(`What would you like to log today? `, async item => {
-  const { data } = await axios.get("http://localhost:3001/food");
-  const it = data[Symbol.iterator]();
-  let pos = it.next();
-  while (!pos.done) {
-    const food = pos.value.name;
-    if (food === item) {
-      console.log(`${item} has ${pos.value.calories} calories`);
-    }
-    pos = it.next();
+readline.prompt();
+readline.on("line", line => {
+  switch (line.trim()) {
+    case "list vegan foods":
+      {
+        axios.get("htto://localhost:3001/food").then(({ data }) => {
+          let idx = 0;
+          const veganIterable = {
+            [Symbol.iterator]() {
+              return {
+                [Symbol.iterator]() {
+                  return this;
+                },
+                next() {
+                  const cur = data[idx];
+                  idx++;
+                  if (cur) {
+                    return { value: cur, done: false };
+                  } else {
+                    return { value: undefined, done: true };
+                  }
+                }
+              };
+            }
+          };
+          for (const vegan of veganIterable) {
+            console.log(vegan.name);
+          }
+
+          readline.prompt();
+        });
+      }
+      break;
+    case "eat":
+      readline.question(`What would you like to log today? `, async item => {
+        const { data } = await axios.get("http://localhost:3001/food");
+        const it = data[Symbol.iterator]();
+        let pos = it.next();
+        while (!pos.done) {
+          const food = pos.value.name;
+          if (food === item) {
+            console.log(`${item} has ${pos.value.calories} calories`);
+          }
+          pos = it.next();
+        }
+        readline.close();
+      });
   }
-  readline.close();
 });
