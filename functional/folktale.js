8811db891ec9bd55fd75183c4e4145aa41d4f5b6
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2020-04-02 22:08:01 +0800

    try folktale

--- functional/folktale.js
+++ functional/folktale.js
@@ -78,3 +78,41 @@ const xx = divide(4, 2)
 console.log(xx);
 const err = divide(2, 0);
 console.log(err);
+
+// Validation
+//  map/mapFailure/matchWith/concat/collect
+
+const F = require("folktale");
+
+const minLength = (fieldName, minLen, value) =>
+  value && value.length && value.length > minLen
+    ? F.validation.Success(value)
+    : F.validation.Failure([
+        `${fieldName} must be more then ${minLen} chars long`
+      ]);
+
+const noSpaces = (fieldName, value) =>
+  value.indexOf(" ") === -1
+    ? F.validation.Success(value)
+    : F.validation.Failure([`${fieldName} cannot contain a space`]);
+
+const pwdValid = pwd =>
+  F.validation
+    .Success()
+    .concat(noSpaces("Password", pwd))
+    .concat(minLength("Password", 5, pwd));
+const nameValid = name => F.validation.Success().concat(noSpaces("Name", name));
+
+console.log(pwdValid(""));
+console.log(pwdValid("abcd"));
+console.log(nameValid("abcd daf"));
+
+console.log("-".repeat(10));
+out = F.validation.collect([pwdValid("ab f"), nameValid("dave")]);
+console.log(out);
+console.log("-".repeat(10));
+out = F.validation.collect([pwdValid("ab f"), nameValid("dave")]).matchWith({
+  Success: ({ value }) => `Success: ${value}`,
+  Failure: ({ value }) => `Failure: ${value}`
+});
+console.log(out);
